e9afc8fa52392a6ba09cef9eee9b5917b3028a85
ClemAMolette <clementinegaga79@outlook.fr>
2024-12-04 16:17:09 +0100

    added pinia store

---
+++ frontend/src/stores/mice.ts
@@ -0,0 +1,37 @@
+import { defineStore } from 'pinia'
+
+export const useMiceStore = defineStore('mice', {
+    state: () => ({
+		mouseBlue: {src:"/squeak_icons/blue_icon.png", path:[]},
+        mouseBlack: {src:"/squeak_icons/black_icon.png", path:[]} 
+	}),
+    actions: {
+		addCoordBlue(coord: Array<number> ) {
+            this.mouseBlue["path"].push(coord);
+        },
+		addCoordBlack(coord: Array<number> ) {
+            this.mouseBlack["path"].push(coord);
+        },
+	},
+	getters: {
+		blueIcon: (state) => {
+			return state.mouseBlue["src"]
+		},
+        blackIcon: (state) => {
+			return state.mouseBlack["src"]
+		},
+		bluePath: (state) => {
+			return state.mouseBlue["path"]
+		},
+		blackPath: (state) => {
+			return state.mouseBlack["path"]
+		},
+		blueLastCoord: (state) => {
+			return state.mouseBlue["path"][-1]
+		},
+		blackLastCoord: (state) => {
+			return state.mouseBlack["path"][-1]
+		},
+		
+	}
+})
