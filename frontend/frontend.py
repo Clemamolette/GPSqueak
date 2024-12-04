728996899d4b6e2588a666e7e1fa228300c19652
MaelTriquet <mael1triquet@gmail.com>
2024-12-04 12:06:56 +0100

    adapted to the fetch_data fix

--- frontend/frontend.py
+++ frontend/frontend.py
@@ -18,16 +18,15 @@ db_connection, cursor = db.connect(db_params)
 def get_all_pos(ip : str) -> dict:
     query = """
         SELECT (coordinates.latitude, coordinates.longitude) FROM coordinates
-        WHERE coordinates.ip = %s
-        """
+        WHERE coordinates.ip = """ + ip
     
-    return db.fetch_data(query, db_connection, cursor, [ip])
+    return db.fetch_data(query, db_connection, cursor)
 
 @app.get("/position/{ip}")
 def get_last_pos(ip : str) -> dict:
     query = """
         SELECT (coordinates.latitude, coordinates.longitude) FROM coordinates
-        WHERE coordinates.ip = %s
+        WHERE coordinates.ip = """ + ip + """
         ORDER BY DESC coordinates.id
         LIMIT 1
         """
