d90cf2ce9408a43d093931d0c11e6c597bd89ac1
FaucherNoe <faucher.noe7@gmail.com>
2024-12-07 12:57:27 +0100

    fix typo and add type in mice store

--- frontend/src/components/Map.vue
+++ frontend/src/components/Map.vue
@@ -31,7 +31,7 @@ export default {
     L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
       attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    }).addTo(map);y
+    }).addTo(map);
 
     var pathBlue = miceStore.bluePath;
     var pathBlack = miceStore.blackPath;
