1c2c02d9593e54776505bca607c848b7580aba77
MaelTriquet <mael1triquet@gmail.com>
2024-12-22 16:59:18 +0100

    fix foreign key

--- database/init.sql
+++ database/init.sql
@@ -1,5 +1,5 @@
 
 
-CREATE TABLE IF NOT EXISTS coordinates(id SERIAL PRIMARY KEY, id_mouse FOREIGN KEY(mouses.id), latitude FLOAT, longitude FLOAT, t_stamp INT);
+CREATE TABLE IF NOT EXISTS coordinates(id SERIAL PRIMARY KEY, FOREIGN KEY(id_mouse) REFERENCES mouses(id), latitude FLOAT, longitude FLOAT, t_stamp INT);
 
 CREATE TABLE IF NOT EXISTS mouses(id SERIAL PRIMARY KEY, ip VARCHAR(50), name VARCHAR(100));
